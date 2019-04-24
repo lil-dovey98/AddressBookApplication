5b12e7a11b01bb50a2647cebfdfc340f127244ce
lil-dovey98 <47420606+lil-dovey98@users.noreply.github.com>
2019-04-24 11:23:26 +0100

    Added Address Class
    - Files added due to first compile
    - Added an Address class to my Model folder
    - Small formatting changes such as headers and footers to show more appropriate values

---
+++ WebApplication1/WebApplication1/Models/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Address
+    {
+        public int ID { get; set; }
+        //database need a unique identifier (primary key)
+        public string Forenames { get; set; }
+        public string Surname { get; set; }
+        public string AddressLineOne { get; set; }
+        public string AddressLineTwo { get; set; }
+        public string Postcode { get; set; }
+        public string Town { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+        //stored as a string to avoid incorrect formatting
+    }
+}
