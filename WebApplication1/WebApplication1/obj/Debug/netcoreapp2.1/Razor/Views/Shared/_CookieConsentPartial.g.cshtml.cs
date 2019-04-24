ed06c72831629b5235a8b77e366c1a8e7d8fc621
lil-dovey98 <47420606+lil-dovey98@users.noreply.github.com>
2019-04-24 13:13:17 +0100

    Formatting
    - Changed the paths of the views to be more logical
    - Changed some labels across various views to be more human readable
    - Removed auto-generated index page and changed startup file to open up to the main screen of the application

--- WebApplication1/WebApplication1/obj/Debug/netcoreapp2.1/Razor/Views/Shared/_CookieConsentPartial.g.cshtml.cs
+++ WebApplication1/WebApplication1/obj/Debug/netcoreapp2.1/Razor/Views/Shared/_CookieConsentPartial.g.cshtml.cs
@@ -97,7 +97,7 @@ using Microsoft.AspNetCore.Http.Features;
                     ");
             EndContext();
             BeginContext(1234, 92, false);
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "db6f3e2b237842a6816be6c4abc26ada", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "0eb80c1d06d34de39e6b9827bb5340ce", async() => {
                 BeginContext(1312, 10, true);
                 WriteLiteral("Learn More");
                 EndContext();
